5e9b830c7afc2dd4e072d43fba527ab6997edcb8
rishikeshs355 <rishikeshs355@gmail.com>
2023-10-13 14:47:24 +0530

    Second Commit

--- server/.env/url.js
+++ server/.env/url.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURL: 'mongodb+srv://rishikeshsinghs355:Te9ZN4u38kfPGny7@bigstack.zqq3tvv.mongodb.net/',
+    mongoURL: '',//write your own url
     secret: "Mysecret",
   }
