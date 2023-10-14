7a54c194c2f81fe3399eb467e3fd1c451df9000b
rishikeshs355 <rishikeshs355@gmail.com>
2023-10-14 10:26:42 +0530

    Commit after adding .gitignore in server

--- server/.env/url.js
+++ server/.env/url.js
@@ -1,4 +1,9 @@
-module.exports = {
-    mongoURL: '',//write your own url
+// module.exports = {
+//     mongoURL: '',//write your own url
+//     secret: "Mysecret",
+//   }
+
+  module.exports = {
+    mongoURL: 'mongodb+srv://rishikeshsinghs355:Te9ZN4u38kfPGny7@bigstack.zqq3tvv.mongodb.net/',
     secret: "Mysecret",
   }
